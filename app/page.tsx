619e0222284018e6407b8d64cd584c02d00f8b74
joshvan1226 <joshvan2@icloud.com>
2026-05-27 13:05:24 -0400

    initial nova os

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function HomePage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#030607",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "3rem",
+        fontWeight: "bold",
+      }}
+    >
+      NOVA OS
+    </main>
+  );
+}
